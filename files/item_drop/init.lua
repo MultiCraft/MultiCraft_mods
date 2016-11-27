6dfa3fee6f4a59af1a21f440a612847269b3de4c
Maksim Gamarnik <MoNTE48@mail.ua>
2016-11-28 01:19:49 +0200

    Update item_drop

--- files/item_drop/init.lua
+++ files/item_drop/init.lua
@@ -1,148 +1,99 @@
--- basic settings
-item_drop_settings = {} -- settings table
-item_drop_settings.age = 1 -- how old an item has to be before collecting
-item_drop_settings.radius_magnet = 2.0 -- radius of item magnet
-item_drop_settings.radius_collect = 0.3 -- radius of collection
-item_drop_settings.player_collect_height = 1.0 -- added to their pos y value
-item_drop_settings.collection_safety = true -- do this to prevent items from flying away on laggy servers
-
-
-minetest.register_globalstep(function (dtime)
-		for _, player in ipairs(minetest.get_connected_players()) do
-			if player:get_hp() > 0 or not minetest.setting_getbool("enable_damage") then
-				local pos = player:getpos()
-				local inv = player:get_inventory()
-
-				-- collection
-
-				for _,object in ipairs(minetest.get_objects_inside_radius({x = pos.x, y = pos.y + item_drop_settings.player_collect_height, z = pos.z}, item_drop_settings.radius_collect)) do
-					if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
-						if object:get_luaentity().collect and object:get_luaentity().age > item_drop_settings.age then
-							if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
-
-								if object:get_luaentity().itemstring ~= "" then
-									inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
-									minetest.sound_play("item_drop_pickup", {
-											pos = pos,
-											max_hear_distance = 25,
-											gain = 0.1,
-										})
-									object:get_luaentity().itemstring = ""
-									object:remove()
-								end
-
-
+--Item collection
+minetest.register_globalstep(function(dtime)
+	--basic settings
+	local age                   = 1 --how old an item has to be before collecting
+	local radius_magnet         = 2.0 --radius of item magnet
+	local radius_collect        = 0.3 --radius of collection
+	local player_collect_height = 1.2 --added to their pos y value
+	for _,player in ipairs(minetest.get_connected_players()) do
+		if player:get_hp() > 0 then
+			local pos = player:getpos()
+			local inv = player:get_inventory()
+			--collection
+			for _,object in ipairs(minetest.env:get_objects_inside_radius({x=pos.x,y=pos.y + player_collect_height,z=pos.z}, radius_collect)) do
+				if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
+					if object:get_luaentity().age > age then
+						if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
+							
+							if object:get_luaentity().itemstring ~= "" then
+								inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
+								minetest.sound_play("item_drop_pickup", {
+									pos = pos,
+									max_hear_distance = 50,
+									gain = 0.1,
+								})
+								object:get_luaentity().itemstring = ""
+								object:remove()
 							end
+							
+							
 						end
 					end
 				end
-
-
-				-- magnet
-				for _,object in ipairs(minetest.get_objects_inside_radius({x = pos.x, y = pos.y + item_drop_settings.player_collect_height, z = pos.z}, item_drop_settings.radius_magnet)) do
-					if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
-						if object:get_luaentity().collect and object:get_luaentity().age > item_drop_settings.age then
-							if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
-
-								-- modified simplemobs api
-
-								local pos1 = pos
-								-- pos1.y = pos1.y+item_drop_settings.player_collect_height
-								local pos2 = object:getpos()
-								local vec = {x = pos1.x - pos2.x, y = (pos1.y + item_drop_settings.player_collect_height) - pos2.y, z = pos1.z - pos2.z}
-								-- vec.x = vec.x
-								-- vec.y = vec.y
-								-- vec.z = vec.z
-
-								vec.x = pos2.x + (vec.x / 3)
-								vec.y = pos2.y + (vec.y / 3)
-								vec.z = pos2.z + (vec.z / 3)
-								object:moveto(vec)
-
-								object:get_luaentity().physical_state = false
-								object:get_luaentity().object:set_properties({
-										physical = false
-									})
-
-								-- fix eternally falling items
-								minetest.after(0, function ()
-										object:setacceleration({x = 0, y = 0, z = 0})
-									end)
-
-
-								-- this is a safety to prevent items flying away on laggy servers
-								if item_drop_settings.collection_safety == true then
-									if object:get_luaentity().init ~= true then
-										object:get_luaentity().init = true
-										minetest.after(1, function (args)
-												local lua = object:get_luaentity()
-												if object == nil or lua == nil or lua.itemstring == nil then
-													return
-												end
-												if inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
-													inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
-													if object:get_luaentity().itemstring ~= "" then
-														minetest.sound_play("item_drop_pickup", {
-																pos = pos,
-																max_hear_distance = 25,
-																gain = 0.1,
-															})
-													end
-													object:get_luaentity().itemstring = ""
-													object:remove()
-												else
-													object:setvelocity({x = 0, y = 0, z = 0})
-													object:get_luaentity().physical_state = true
-													object:get_luaentity().object:set_properties({
-															physical = true
-														})
-												end
-											end, {player, object})
-									end
-								end
-							end
+			end
+			--magnet
+			for _,object in ipairs(minetest.env:get_objects_inside_radius({x=pos.x,y=pos.y + player_collect_height,z=pos.z}, radius_magnet)) do
+				if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
+					if object:get_luaentity().age > age then
+						if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
+							local pos1 = pos
+							local pos2 = object:getpos()
+							local vec = {x=pos1.x-pos2.x, y=(pos1.y+player_collect_height)-pos2.y, z=pos1.z-pos2.z}
+							vec.x = pos2.x + (vec.x/2)
+							vec.y = pos2.y + (vec.y/2)
+							vec.z = pos2.z + (vec.z/2)
+							object:moveto(vec)
+							object:get_luaentity().physical_state = false
+							object:get_luaentity().object:set_properties({
+								physical = false
+							})
 						end
 					end
 				end
 			end
 		end
-	end)
-
-function minetest.handle_node_drops(pos, drops, digger)
-	local inv
-	if minetest.setting_getbool("creative_mode") and digger and digger:is_player() then
-		inv = digger:get_inventory()
 	end
-	for _, item in ipairs(drops) do
-		local count, name
-		if type(item) == "string" then
-			count = 1
-			name = item
-		else
-			count = item:get_count()
-			name = item:get_name()
+end)
+
+--Drop items on dig
+--This only works in survival
+if minetest.setting_getbool("creative_mode") == false then
+	function minetest.handle_node_drops(pos, drops, digger)
+		local inv
+		if minetest.setting_getbool("creative_mode") and digger and digger:is_player() then
+			inv = digger:get_inventory()
 		end
-		if not inv or not inv:contains_item("main", ItemStack(name)) then
-			for i = 1, count do
-				local obj = minetest.add_item(pos, name)
-				if obj ~= nil then
-					-- obj:get_luaentity().timer =
-					obj:get_luaentity().collect = true
-					local x = math.random(1, 5)
-					if math.random(1, 2) == 1 then
-						x = -x
-					end
-					local z = math.random(1, 5)
-					if math.random(1, 2) == 1 then
-						z = -z
-					end
-					obj:setvelocity({x = 1 / x, y = obj:getvelocity().y, z = 1 / z})
-					obj:get_luaentity().age = 0.6
-					-- FIXME this doesnt work for deactiveted objects
-					if minetest.setting_get("remove_items") and tonumber(minetest.setting_get("remove_items")) then
-						minetest.after(tonumber(minetest.setting_get("remove_items")), function (obj)
+		for _,item in ipairs(drops) do
+			local count, name
+			if type(item) == "string" then
+				count = 1
+				name = item
+			else
+				count = item:get_count()
+				name = item:get_name()
+			end
+			if not inv or not inv:contains_item("main", ItemStack(name)) then
+				for i=1,count do
+					local obj = minetest.env:add_item(pos, name)
+					if obj ~= nil then
+						--obj:get_luaentity().timer = 
+						obj:get_luaentity().collect = true
+						local x = math.random(1, 5)
+						if math.random(1,2) == 1 then
+							x = -x
+						end
+						local z = math.random(1, 5)
+						if math.random(1,2) == 1 then
+							z = -z
+						end
+						obj:setvelocity({x=1/x, y=obj:getvelocity().y, z=1/z})
+						obj:get_luaentity().age = 0.6
+						-- FIXME this doesnt work for deactiveted objects
+						if minetest.setting_get("remove_items") and tonumber(minetest.setting_get("remove_items")) then
+							minetest.after(tonumber(minetest.setting_get("remove_items")), function(obj)
 								obj:remove()
 							end, obj)
+						end
 					end
 				end
 			end
@@ -150,54 +101,51 @@ function minetest.handle_node_drops(pos, drops, digger)
 	end
 end
 
--- throw single items by default
+--throw items using player's velocity
 function minetest.item_drop(itemstack, dropper, pos)
-	if dropper and dropper:is_player() then
+	--do this to prevent dispensers and other things using this function from breaking
+	
+	--check if player
+	local is_player = false
+	if dropper then
+		if minetest.get_player_information(dropper:get_player_name()) then
+			is_player = true
+		end
+	end
+	--if player then do modified item drop
+	if dropper and is_player == true then
 		local v = dropper:get_look_dir()
-		local p = {x = pos.x, y = pos.y + 1.2, z = pos.z}
-		local cs = 1
+		local vel = dropper:get_player_velocity()
+		local p = {x=pos.x, y=pos.y+1.3, z=pos.z}
+
+		local item = itemstack:to_string()
+		local obj = core.add_item(p, item)
+		if obj then
+			v.x = (v.x*5)+vel.x
+			v.y = ((v.y*5)+2)+vel.y
+			v.z = (v.z*5)+vel.z
+			obj:setvelocity(v)
+			obj:get_luaentity().dropped_by = dropper:get_player_name()
+			--obj:get_luaentity().collect = true
+			itemstack:clear()
+			return itemstack
+		end
+	else --if machine then use default item drop to not break mods - also extend reach
+		local v = dropper:get_look_dir()
+		local p = {x=pos.x+v.x, y=pos.y+1.5+v.y, z=pos.z+v.z}
+		local cs = itemstack:get_count()
 		if dropper:get_player_control().sneak then
-			cs = itemstack:get_count()
+			cs = 1
 		end
 		local item = itemstack:take_item(cs)
 		local obj = core.add_item(p, item)
 		if obj then
-			v.x = v.x * 4
-			v.y = v.y * 4 + 2
-			v.z = v.z * 4
+			v.x = v.x*5
+			v.y = v.y*5 + 2
+			v.z = v.z*5
 			obj:setvelocity(v)
-			obj:get_luaentity().collect = true
+			obj:get_luaentity().dropped_by = dropper:get_player_name()
 			return itemstack
 		end
-	end
-end
-
--- add food particles
-function core.item_eat(hp_change, replace_with_item)
-	return function (itemstack, user, pointed_thing) -- closure
-		local pos = user:getpos()
-		pos.y = pos.y + item_drop_settings.player_collect_height + 0.5
-		local itemname = itemstack:get_name()
-		local texture = minetest.registered_items[itemname].inventory_image
-		minetest.add_item(pos, drop)
-		minetest.add_particlespawner({
-				amount = 20,
-				time = 0.1,
-				minpos = {x = pos.x, y = pos.y, z = pos.z},
-				maxpos = {x = pos.x, y = pos.y, z = pos.z},
-				minvel = {x = -1, y = 1, z = -1},
-				maxvel = {x = 1, y = 2, z = 1},
-				minacc = {x = 0, y = -5, z = 0},
-				maxacc = {x = 0, y = -9, z = 0},
-				minexptime = 1,
-				maxexptime = 1,
-				minsize = 1,
-				maxsize = 2,
-				collisiondetection = true,
-				vertical = false,
-				texture = texture,
-			})
-
-		return core.do_item_eat(hp_change, replace_with_item, itemstack, user, pointed_thing)
 	end
 end
