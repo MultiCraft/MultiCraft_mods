9fd4d3e61f9959049e8b89294c23905ea1ce65dc
MultiCraft <support@multicraft.world>
2018-05-27 20:53:09 +0200

    Update and compress textures

--- files/sethome/init.lua
+++ files/sethome/init.lua
@@ -5,26 +5,12 @@ local homes_file = minetest.get_worldpath() .. "/homes"
 local homepos = {}
 
 local function loadhomes()
---	local input = io.open(homes_file, "r")
---	if input then
---		repeat
---			local x = input:read("*n")
---			if x == nil then
---				break
---			end
---			local y = input:read("*n")
---			local z = input:read("*n")
---			local name = input:read("*l")
---			homepos[name:sub(2)] = {x = x, y = y, z = z}
---		until input:read(0) == nil
---		io.close(input)
---	end
-	local input, err = io.open(homes_file, "r")
+	local input = io.open(homes_file, "r")
 	if not input then
-		return minetest.log("info", "Could not load player homes file: " .. err)
+		return -- no longer an error
 	end
 
-	-- Iterate over all stored positions in the format "x y z player" each line
+	-- Iterate over all stored positions in the format "x y z player" for each line
 	for pos, name in input:read("*a"):gmatch("(%S+ %S+ %S+)%s([%w_-]+)[\r\n]") do
 		homepos[name] = minetest.string_to_pos(pos)
 	end
@@ -38,11 +24,13 @@ sethome.set = function(name, pos)
 	if not player or not pos then
 		return false
 	end
+	player:set_attribute("sethome:home", minetest.pos_to_string(pos))
 
+	-- remove `name` from the old storage file
 	local data = {}
-	local output, err = io.open(homes_file, "w")
+	local output = io.open(homes_file, "w")
 	if output then
-		homepos[name] = pos
+		homepos[name] = nil
 		for i, v in pairs(homepos) do
 			table.insert(data, string.format("%.1f %.1f %.1f %s\n", v.x, v.y, v.z, i))
 		end
@@ -50,24 +38,39 @@ sethome.set = function(name, pos)
 		io.close(output)
 		return true
 	end
-	minetest.log("action", "Unable to write to player homes file: " .. err)
-	return false
+	return true -- if the file doesn't exist - don't return an error.
 end
 
 sethome.get = function(name)
-	return homepos[name]
+	local player = minetest.get_player_by_name(name)
+	local pos = minetest.string_to_pos(player:get_attribute("sethome:home"))
+	if pos then
+		return pos
+	end
+
+	-- fetch old entry from storage table
+	pos = homepos[name]
+	if pos then
+		return vector.new(pos)
+	else
+		return nil
+	end
 end
 
 sethome.go = function(name)
+	local pos = sethome.get(name)
 	local player = minetest.get_player_by_name(name)
-	if player and homepos[name] then
-		player:setpos(homepos[name])
+	if player and pos then
+		player:setpos(pos)
 		return true
 	end
 	return false
 end
 
-minetest.register_privilege("home", "Can use /sethome and /home")
+minetest.register_privilege("home", {
+	description = "Can use /sethome and /home",
+	give_to_singleplayer = true
+})
 
 minetest.register_chatcommand("home", {
 	description = "Teleport you to your home point",
