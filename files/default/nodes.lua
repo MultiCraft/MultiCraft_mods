8d004f9396cc35444ceef63e1f09866329b2e963
Amaz <Amaz1@users.noreply.github.com>
2018-08-02 08:03:52 +0100

    Add signs with visible text (#125)
    * Add signs with visible text
    
    * Compress signs textures (-70.6%)
    
    * Add lbm to convert old signs to new ones
    
    * More sensible approach in legacy
    
    * Check for sign text on block load
    Missing text can also be fixed by punching the sign.
    
    * Finally, detect sign type on placement
    
    * Neaten code up slightly

--- files/default/nodes.lua
+++ files/default/nodes.lua
@@ -891,55 +891,6 @@ minetest.register_node("default:bookshelf", {
     sounds = default.node_sound_wood_defaults(),
 })
 
-local function register_sign(material, desc, def)
-	minetest.register_node("default:sign_wall_" .. material, {
-		description = desc .. " Sign",
-		drawtype = "nodebox",
-		tiles = {"default_sign_wall_" .. material .. ".png"},
-		inventory_image = "default_sign_" .. material .. ".png",
-		wield_image = "default_sign_" .. material .. ".png",
-		paramtype = "light",
-		paramtype2 = "wallmounted",
-		sunlight_propagates = true,
-		is_ground_content = false,
-		walkable = false,
-		node_box = {
-			type = "wallmounted",
-			wall_top    = {-0.4375, 0.4375, -0.3125, 0.4375, 0.5, 0.3125},
-			wall_bottom = {-0.4375, -0.5, -0.3125, 0.4375, -0.4375, 0.3125},
-			wall_side   = {-0.5, -0.3125, -0.4375, -0.4375, 0.3125, 0.4375},
-		},
-		groups = def.groups,
-		legacy_wallmounted = true,
-		sounds = def.sounds,
-
-		on_construct = function(pos)
-			--local n = minetest.get_node(pos)
-			local meta = minetest.get_meta(pos)
-			meta:set_string("formspec", "field[text;;${text}]")
-		end,
-		on_receive_fields = function(pos, formname, fields, sender)
-			--print("Sign at "..minetest.pos_to_string(pos).." got "..dump(fields))
-			local player_name = sender:get_player_name()
-			if minetest.is_protected(pos, player_name) then
-				minetest.record_protection_violation(pos, player_name)
-				return
-			end
-			local meta = minetest.get_meta(pos)
-			if not fields.text then return end
-			minetest.log("action", (player_name or "") .. " wrote \"" ..
-				fields.text .. "\" to sign at " .. minetest.pos_to_string(pos))
-			meta:set_string("text", fields.text)
-			meta:set_string("infotext", '"' .. fields.text .. '"')
-		end,
-	})
-end
-
-register_sign("wood", "Wooden", {
-	sounds = default.node_sound_wood_defaults(),
-	groups = {choppy = 2, attached_node = 1, flammable = 2, oddly_breakable_by_hand = 3}
-})
-
 minetest.register_node("default:slimeblock", {
     description = "Slime Block",
     drawtype = "nodebox",
