402f9d6ba777fe71b2e4d9569d1b2e965f234b0c
MultiCraft <support@multicraft.world>
2018-08-31 23:21:40 +0200

    Move killme to builtin, TNT fix and new chicken model by @stujones11

--- files/hunger/init.lua
+++ files/hunger/init.lua
@@ -34,18 +34,19 @@ hud.load_hunger = hunger.load
 if minetest.settings:get_bool("enable_damage") then
 
 	minetest.register_on_joinplayer(function(player)
-		local inv = player:get_inventory()
-		inv:set_size("hunger", 1)
-
-		local name = player:get_player_name()
-		hunger.players[name] = {}
-		hunger.players[name].lvl = hunger.read(player)
-		hunger.players[name].exhaus = 0
-		local lvl = hunger.players[name].lvl
-		if lvl > 20 then
-			lvl = 20
-		end
 		minetest.after(1, function()
+			local inv = player:get_inventory()
+			inv:set_size("hunger", 1)
+
+			local name = player:get_player_name()
+			hunger.players[name] = {}
+			hunger.players[name].lvl = hunger.read(player)
+			hunger.players[name].exhaus = 0
+			local lvl = hunger.players[name].lvl
+			if lvl > 20 then
+				lvl = 20
+			end
+
 			hud.swap_statbar(player, "hunger", "air")
 			hud.change_item(player, "hunger", {number = lvl, max = 20})
 		end)
