192148e05d5085967ee2a0f4a75893a969f9dd42
MultiCraft <support@multicraft.world>
2019-06-11 23:50:38 +0200

    WIP: Cleanup and fixes, stairs no longer registers new nodes, only API. Workbench adds blocks to creative inventory

--- files/hud/init.lua
+++ files/hud/init.lua
@@ -1,10 +1,11 @@
 hud = {}
 
+hud.show_armor = minetest.get_modpath("3d_armor") ~= nil
+
 if minetest.settings:get_bool("enable_damage") then
 
 	local modpath = minetest.get_modpath("hud")
 	dofile(modpath .. "/api.lua")
 	dofile(modpath .. "/builtin.lua")
-	dofile(modpath .. "/legacy.lua")
 
 end
