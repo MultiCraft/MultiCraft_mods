cd1754114724b99503eaacd38e25bef0b75ccf69
MultiCraft <support@multicraft.world>
2019-06-15 23:44:20 +0200

    Add default gui_bg and listcolors. Survival inv no longer uses sfinv

--- files/sfinv/init.lua
+++ files/sfinv/init.lua
@@ -1,11 +1,7 @@
 dofile(minetest.get_modpath("sfinv") .. "/api.lua")
 
 sfinv.register_page("sfinv:inventory", {
-	title = "Inventory",
 	get = function(self, player, context)
-		return sfinv.make_formspec(player, context, [[
-				list[current_player;craft;1.75,0.5;3,3;]
-				list[current_player;craftpreview;5.75,1.5;1,1;]
-			]], true)
+		return sfinv.make_formspec(player, context, [[ ]], true)
 	end
 })
