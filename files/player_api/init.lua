b2f014fac07e8f91aab29d69571647b780b067fe
stujones11 <stujones111@gmail.com>
2019-07-01 21:39:30 +0100

    Add cube wieldview and other minor fixes

--- files/player_api/init.lua
+++ files/player_api/init.lua
@@ -9,7 +9,7 @@ end
 -- Default player appearance
 player_api.register_model("character.b3d", {
 	animation_speed = 30,
-	textures = {"character.png", "blank.png", "blank.png"},
+	textures = {"character.png", "blank.png", "blank.png", "blank.png"},
 	animations = {
 		-- Standard animations.
 		stand     = {x = 0,   y = 0}, -- y = 79
