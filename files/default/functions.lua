444355ca067872177853d8df79f61ad0fc33eda2
MultiCraft <support@multicraft.world>
2019-07-08 18:27:09 +0200

    Add walls mod (for compatibility only)

--- files/default/functions.lua
+++ files/default/functions.lua
@@ -488,17 +488,17 @@ minetest.register_abm({
 
 local moss_correspondences = {
 	["default:cobble"] = "default:mossycobble",
-	["stairs:slab_cobble"] = "stairs:slab_mossycobble",
-	["stairs:stair_cobble"] = "stairs:stair_mossycobble",
+	["stairs:slab_default_cobble"] = "stairs:slab_mossycobble",
+	["stairs:stair_default_cobble"] = "stairs:stair_mossycobble",
 	["stairs:stair_innerstair_cobble"] = "stairs:stair_innerstair_mossycobble",
 	["stairs:stair_outerstair_cobble"] = "stairs:stair_outerstair_mossycobble",
---	["walls:cobble"] = "walls:mossycobble",
+	["walls:cobble"] = "walls:mossycobble"
 }
 minetest.register_abm({
 	label = "Moss growth",
-	nodenames = {"default:cobble", "stairs:slab_cobble", "stairs:stair_cobble",
+	nodenames = {"default:cobble", "stairs:slab_default_cobble", "stairs:stair_default_cobble",
 		"stairs:stair_innerstair_cobble", "stairs:stair_outerstair_cobble",
---		"walls:cobble"
+		"walls:cobble"
 	},
 	neighbors = {"group:water"},
 	interval = 16,
