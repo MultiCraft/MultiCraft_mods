cf58c6f1d46e6f375f0fdca478e73cc93eca2ee8
MoNTE48 <MoNTE48@mail.ua>
2019-07-27 18:28:17 +0200

    Move split_inv in default, bones can be found in dirt, other minor things

--- files/farming/hoes.lua
+++ files/farming/hoes.lua
@@ -1,4 +1,4 @@
-farming.register_hoe(":farming:hoe_wood", {
+farming.register_hoe("farming:hoe_wood", {
 	description = "Wooden Hoe",
 	inventory_image = "farming_tool_woodhoe.png",
 	max_uses = 30,
@@ -6,28 +6,28 @@ farming.register_hoe(":farming:hoe_wood", {
 	groups = {flammable = 2},
 })
 
-farming.register_hoe(":farming:hoe_stone", {
+farming.register_hoe("farming:hoe_stone", {
 	description = "Stone Hoe",
 	inventory_image = "farming_tool_stonehoe.png",
 	max_uses = 90,
 	material = "group:stone"
 })
 
-farming.register_hoe(":farming:hoe_steel", {
+farming.register_hoe("farming:hoe_steel", {
 	description = "Steel Hoe",
 	inventory_image = "farming_tool_steelhoe.png",
 	max_uses = 500,
 	material = "default:steel_ingot"
 })
 
-farming.register_hoe(":farming:hoe_gold", {
+farming.register_hoe("farming:hoe_gold", {
 	description = "Gold Hoe",
 	inventory_image = "farming_tool_goldhoe.png",
 	max_uses = 350,
 	material = "default:gold_ingot"
 })
 
-farming.register_hoe(":farming:hoe_diamond", {
+farming.register_hoe("farming:hoe_diamond", {
 	description = "Diamond Hoe",
 	inventory_image = "farming_tool_diamondhoe.png",
 	max_uses = 1000,
