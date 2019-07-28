d9d9f976ac0c0e374bd2897f58c06e1f0142f0c5
MultiCraft <support@multicraft.world>
2019-07-29 00:59:38 +0200

    Add translations, Game translated into Russian

--- files/sethome/init.lua
+++ files/sethome/init.lua
@@ -1,8 +1,11 @@
 sethome = {}
 
+-- Intllib
+local S = intllib.make_gettext_pair()
+
 sethome.set = function(name, pos)
 	local player = minetest.get_player_by_name(name)
-	if not player or not pos then
+	if not player or not minetest.is_valid_pos(pos) then
 		return false
 	end
 	player:set_attribute("sethome:home", minetest.pos_to_string(pos))
@@ -39,9 +42,9 @@ minetest.register_chatcommand("home", {
 	description = "Teleport you to your home point",
 	func = function(name)
 		if sethome.go(name) then
-			return true, green("Teleported to home!")
+			return true, green(S("Teleported to home!"))
 		end
-		return false, red("Set a home using /sethome")
+		return false, red(S("Set a home using /sethome"))
 	end,
 })
 
@@ -51,9 +54,9 @@ minetest.register_chatcommand("sethome", {
 		name = name or "" -- fallback to blank name if nil
 		local player = minetest.get_player_by_name(name)
 		if player and sethome.set(name, player:get_pos()) then
-			return true, green("Home set!")
+			return true, green(S("Home set!"))
 		end
-		return false, red("Player not found!")
+		return false, red(S("Player not found!"))
 	end,
 })
 
@@ -64,13 +67,13 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 	local player_name = player:get_player_name()
 	if fields.sethome_set then
 		sethome.set(player_name, player:get_pos())
-		minetest.chat_send_player(player_name, green("Home set!"))
+		minetest.chat_send_player(player_name, green(S("Home set!")))
 	elseif fields.sethome_go then
 		if sethome.go(player_name) then
 			sethome.go(player_name)
-			minetest.chat_send_player(player_name, green("Teleported to home!"))
+			minetest.chat_send_player(player_name, green(S("Teleported to home!")))
 		else
-			minetest.chat_send_player(player_name, red("Home is not set!"))
+			minetest.chat_send_player(player_name, red(S("Home is not set!")))
 		end
 	end
 end)
