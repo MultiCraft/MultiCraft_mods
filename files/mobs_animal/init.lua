ab00cd869be51a4949e8a5cd5e74e93716788714
MoNTE48 <Maksym48@protonmail.com>
2019-08-10 22:58:13 +0200

    Add Parrot mob

--- files/mobs_animal/init.lua
+++ files/mobs_animal/init.lua
@@ -8,3 +8,4 @@ dofile(path .. "/dog.lua") -- KrupnoPavel
 dofile(path .. "/kitten.lua") -- Jordach/BFD
 dofile(path .. "/pig.lua") -- -- Warthog(Boar) by KrupnoPavel (MIT). Changed to Boar and tweaked by Kaadmy (WTFPL) and MoNTE48 (LGPLv3)
 dofile(path .. "/sheep.lua") -- PilzAdam
+dofile(path .. "/parrot.lua") -- MultiCraft
