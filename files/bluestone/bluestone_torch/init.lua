ffdc219294fbfe35eb97e6a1145c4df19f62930d
MoNTE48 <Maksym48@protonmail.com>
2019-08-11 19:09:58 +0200

    Small fixes in torch, beds, itemframes, sfinv

--- files/bluestone/bluestone_torch/init.lua
+++ files/bluestone/bluestone_torch/init.lua
@@ -10,17 +10,20 @@ default.register_torch("bluestone_torch:torch", {
 	light_source = 10,
 	groups = {choppy = 2, dig_immediate = 3, flammable = 1, attached_node = 1, torch = 1},
 	sounds = default.node_sound_wood_defaults(),
+
 	mesecons = {receptor = {
 		state = mesecon.state.on
 	}},
+
 	on_blast = mesecon.on_blastnode
 })
 
 minetest.register_craft({
 	output = "bluestone_torch:torch 4",
 	recipe = {
-	{"mesecons:wire_00000000_off"},
-	{"default:stick"},}
+		{"mesecons:wire_00000000_off"},
+		{"default:stick"}
+	}
 })
 
 minetest.register_alias("mesecons_torch:mesecon_torch_off", "bluestone_torch:torch")
