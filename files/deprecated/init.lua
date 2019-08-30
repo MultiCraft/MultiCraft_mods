ee7f7b7694dfff5b0e8d48f58438e7a2ee22fdb7
MoNTE48 <Maksym48@protonmail.com>
2019-08-30 23:48:48 +0200

    Moving deprecated stuffs to deprecated mod and fixing a license

---
+++ files/deprecated/init.lua
@@ -0,0 +1,7 @@
+-- Media and code needed to upgrade to the new version.
+-- Must be removed no earlier than 12 months after release.
+
+local path = minetest.get_modpath("deprecated")
+
+--== mesecons_pistons ==--
+dofile(path .. "/mesecons_pistons.lua")
