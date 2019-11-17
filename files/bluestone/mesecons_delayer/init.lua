2af66530194dd15bc7d3c18c51399e9b4c1d3a88
MoNTE48 <MoNTE48@mail.ua>
2019-11-17 22:26:59 +0100

    Update Bluestone Delayer

--- files/bluestone/mesecons_delayer/init.lua
+++ files/bluestone/mesecons_delayer/init.lua
@@ -33,50 +33,65 @@ end
 
 -- Register the 2 (states) x 4 (delay times) delayers
 
+local delaytime = { 0.1, 0.3, 0.5, 1.0 }
+
 for i = 1, 4 do
-local groups = {}
-if i == 1 then 
-	groups = {bendy = 2, snappy = 1, dig_immediate = 2, attached_node = 1}
-else
-	groups = {bendy = 2, snappy = 1, dig_immediate = 2, attached_node = 1, not_in_creative_inventory = 1}
-end
+	local boxes
+	if i == 1 then
+		boxes = {
+			{ -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 },	 -- the main slab
+			{  6/16, -6/16, -1/16, 4/16, -1/16, 1/16 },	 -- still torch
+			{  0/16, -6/16, -1/16, 2/16, -1/16, 1/16 }	 -- moved torch
+		}
+	elseif i == 2 then
+		boxes = {
+			{ -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 },	 -- the main slab
+			{  6/16, -6/16, -1/16, 4/16, -1/16, 1/16 },	 -- still torch
+			{ -2/16, -6/16, -1/16, 0/16, -1/16, 1/16 }	 -- moved torch
+		}
+	elseif i == 3 then
+		boxes = {
+			{ -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 },	 -- the main slab
+			{  6/16, -6/16, -1/16, 4/16, -1/16, 1/16 },	 -- still torch
+			{ -4/16, -6/16, -1/16, -2/16, -1/16, 1/16}	 -- moved torch
+		}
+	elseif i == 4 then
+		boxes = {
+			{ -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 },	 -- the main slab
+			{  6/16, -6/16, -1/16, 4/16, -1/16, 1/16 },	 -- still torch
+			{ -6/16, -6/16, -1/16, -4/16, -1/16, 1/16}	 -- moved torch
+		}
+	end
 
-local delaytime
-if		i == 1 then delaytime = 0.1
-elseif	i == 2 then delaytime = 0.3
-elseif	i == 3 then delaytime = 0.5
-elseif	i == 4 then delaytime = 1.0 end
-
-local boxes
-if i == 1 then
-boxes = {
-	{ -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 },	 -- the main slab
-	{ 6/16, -6/16, -1/16, 4/16, -1/16, 1/16},	 -- still torch
-	{ 0/16, -6/16, -1/16, 2/16, -1/16, 1/16}	 -- moved torch
-}
-elseif i == 2 then
-boxes = {
-	{ -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 },	 -- the main slab
-	{ 6/16, -6/16, -1/16, 4/16, -1/16, 1/16},	 -- still torch
-	{ -2/16, -6/16, -1/16, 0/16, -1/16, 1/16}	 -- moved torch
-}
-elseif i == 3 then
-boxes = {
-	{ -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 },	 -- the main slab
-	{ 6/16, -6/16, -1/16, 4/16, -1/16, 1/16},	 -- still torch
-	{ -4/16, -6/16, -1/16, -2/16, -1/16, 1/16}	 -- moved torch
-}
-elseif i == 4 then
-boxes = {
-	{ -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 },	 -- the main slab
-	{ 6/16, -6/16, -1/16, 4/16, -1/16, 1/16},	 -- still torch
-	{ -6/16, -6/16, -1/16, -4/16, -1/16, 1/16}	 -- moved torch
-}
-end
-
-minetest.register_node("mesecons_delayer:delayer_off_"..tostring(i), {
-	description = "Delayer",
+-- Delayer definition defaults
+local def = {
 	drawtype = "nodebox",
+	selection_box = {
+		type = "fixed",
+		fixed = { -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 }
+	},
+	node_box = {
+		type = "fixed",
+		fixed = boxes
+	},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	is_ground_content = false,
+	delayer_time = delaytime[i],
+	sounds = default.node_sound_stone_defaults(),
+	on_blast = mesecon.on_blastnode,
+	drop = "mesecons_delayer:delayer_off_1"
+}
+
+-- Deactivated delayer definition defaults
+local off_groups = {bendy = 2, snappy = 1, dig_immediate = 2, attached_node = 1}
+if i > 1 then
+	off_groups.not_in_creative_inventory = 1
+end
+
+local off_state = {
+	description = "Delayer",
 	tiles = {
 		"mesecons_delayer_off.png",
 		"default_stone.png",
@@ -86,34 +101,18 @@ minetest.register_node("mesecons_delayer:delayer_off_"..tostring(i), {
 		"mesecons_delayer_sides_off.png"
 	},
 	wield_image = "mesecons_delayer_off.png",
-	selection_box = {
-		type = "fixed",
-		fixed = { -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 }
-	},
-	node_box = {
-		type = "fixed",
-		fixed = boxes
-	},
-	groups = groups,
-	paramtype = "light",
-	paramtype2 = "facedir",
-	sunlight_propagates = true,
-	is_ground_content = false,
-	drop = "mesecons_delayer:delayer_off_1",
-	on_punch = function (pos, node)
-		if node.name=="mesecons_delayer:delayer_off_1" then
-			minetest.swap_node(pos, {name = "mesecons_delayer:delayer_off_2", param2 = node.param2})
-		elseif node.name=="mesecons_delayer:delayer_off_2" then
-			minetest.swap_node(pos, {name = "mesecons_delayer:delayer_off_3", param2 = node.param2})
-		elseif node.name=="mesecons_delayer:delayer_off_3" then
-			minetest.swap_node(pos, {name = "mesecons_delayer:delayer_off_4", param2 = node.param2})
-		elseif node.name=="mesecons_delayer:delayer_off_4" then
-			minetest.swap_node(pos, {name = "mesecons_delayer:delayer_off_1", param2 = node.param2})
+	groups = off_groups,
+	on_punch = function(pos, node, puncher)
+		if minetest.is_protected(pos, puncher and puncher:get_player_name()) then
+			return
 		end
+
+		minetest.swap_node(pos, {
+			name = "mesecons_delayer:delayer_off_" .. tostring(i % 4 + 1),
+			param2 = node.param2
+		})
 	end,
-	delayer_time = delaytime,
-	delayer_onstate = "mesecons_delayer:delayer_on_"..tostring(i),
-	sounds = default.node_sound_stone_defaults(),
+	delayer_onstate = "mesecons_delayer:delayer_on_" .. tostring(i),
 	mesecons = {
 		receptor =
 		{
@@ -125,13 +124,17 @@ minetest.register_node("mesecons_delayer:delayer_off_"..tostring(i), {
 			rules = delayer_get_input_rules,
 			action_on = delayer_activate
 		}
-	},
-	on_blast = mesecon.on_blastnode
-})
+	}
+}
 
+for k, v in pairs(def) do
+	off_state[k] = off_state[k] or v
+end
 
-minetest.register_node("mesecons_delayer:delayer_on_"..tostring(i), {
-	drawtype = "nodebox",
+minetest.register_node("mesecons_delayer:delayer_off_" .. tostring(i), off_state)
+
+-- Activated delayer definition defaults
+local on_state = {
 	tiles = {
 		"mesecons_delayer_on.png",
 		"default_stone.png",
@@ -140,34 +143,18 @@ minetest.register_node("mesecons_delayer:delayer_on_"..tostring(i), {
 		"mesecons_delayer_sides_on.png",
 		"mesecons_delayer_sides_on.png"
 	},
-	selection_box = {
-		type = "fixed",
-		fixed = { -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 }
-	},
-	node_box = {
-		type = "fixed",
-		fixed = boxes
-	},
 	groups = {bendy = 2, snappy = 1, dig_immediate = 2, not_in_creative_inventory = 1},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	sunlight_propagates = true,
-	is_ground_content = false,
-	drop = "mesecons_delayer:delayer_off_1",
-	on_punch = function (pos, node)
-		if node.name=="mesecons_delayer:delayer_on_1" then
-			minetest.swap_node(pos, {name = "mesecons_delayer:delayer_on_2", param2 = node.param2})
-		elseif node.name=="mesecons_delayer:delayer_on_2" then
-			minetest.swap_node(pos, {name = "mesecons_delayer:delayer_on_3", param2 = node.param2})
-		elseif node.name=="mesecons_delayer:delayer_on_3" then
-			minetest.swap_node(pos, {name = "mesecons_delayer:delayer_on_4", param2 = node.param2})
-		elseif node.name=="mesecons_delayer:delayer_on_4" then
-			minetest.swap_node(pos, {name = "mesecons_delayer:delayer_on_1", param2 = node.param2})
+	on_punch = function(pos, node, puncher)
+		if minetest.is_protected(pos, puncher and puncher:get_player_name()) then
+			return
 		end
+
+		minetest.swap_node(pos, {
+			name = "mesecons_delayer:delayer_on_" .. tostring(i % 4 + 1),
+			param2 = node.param2
+		})
 	end,
-	delayer_time = delaytime,
-	delayer_offstate = "mesecons_delayer:delayer_off_"..tostring(i),
-	sounds = default.node_sound_stone_defaults(),
+	delayer_offstate = "mesecons_delayer:delayer_off_" .. tostring(i),
 	mesecons = {
 		receptor =
 		{
@@ -179,9 +166,14 @@ minetest.register_node("mesecons_delayer:delayer_on_"..tostring(i), {
 			rules = delayer_get_input_rules,
 			action_off = delayer_deactivate
 		}
-	},
-	on_blast = mesecon.on_blastnode
-})
+	}
+}
+
+for k, v in pairs(def) do
+	on_state[k] = on_state[k] or v
+end
+
+minetest.register_node("mesecons_delayer:delayer_on_" .. tostring(i), on_state)
 end
 
 minetest.register_craft({
