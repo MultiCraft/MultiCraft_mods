9bf96b4c1b7439f7ab312b03c38d1d994ed8c8dc
MultiCraft <support@multicraft.world>
2020-03-07 17:06:46 +0100

    Fix sandstone and ruby crafting

--- files/deprecated/init.lua
+++ files/deprecated/init.lua
@@ -15,11 +15,16 @@ minetest.register_entity(":throwing:arrow_entity", {
 })
 
 --== split ==--
-minetest.register_on_joinplayer(function()
-	local split_inv = minetest.create_detached_inventory("split", {
-		allow_put = function(_, _, _, stack, _)
-			return stack:get_count() / 2
-		end
-	})
-	split_inv:set_size("main", 1)	
-end)
+local split_inv = minetest.create_detached_inventory("split", {
+	allow_put = function(_, _, _, stack, _)
+		return stack:get_count() / 2
+	end
+})
+split_inv:set_size("main", 1)	
+
+-- Fences alias
+minetest.register_alias("fences:fence_wood", "default:fence_wood")
+for _, n in pairs({"1", "2", "3", "11", "12", "13", "14",
+		"21", "22", "23", "24", "32", "33", "34", "35"}) do
+	minetest.register_alias("fences:fence_wood_" .. n, "default:fence_wood")
+end
