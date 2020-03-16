858055acc4785e0559db6a51b0687f309d6b9217
MoNTE48 <MoNTE48@mail.ua>
2020-03-16 18:47:56 +0100

    Bluestone mesecons_solarpanel: update

--- files/bluestone/mesecons_solarpanel/init.lua
+++ files/bluestone/mesecons_solarpanel/init.lua
@@ -1,51 +1,33 @@
 -- Solar Panel
-minetest.register_node("mesecons_solarpanel:solar_panel_on", {
-	drawtype = "nodebox",
-	tiles = { "jeija_solar_panel.png" },
-	inventory_image = "jeija_solar_panel.png",
-	wield_image = "jeija_solar_panel.png",
-	paramtype = "light",
-	paramtype2 = "wallmounted",
-	is_ground_content = false,
-	node_box = {
-		type = "wallmounted",
-		wall_bottom	= { -8/16, -8/16, -8/16,  8/16, -2/16, 8/16 },
-		wall_top	= { -8/16,  2/16, -8/16,  8/16,  8/16, 8/16 },
-		wall_side	= { -2/16, -8/16, -8/16, -8/16,  8/16, 8/16 }
-	},
-	drop = "mesecons_solarpanel:solar_panel_off",
-	groups = {dig_immediate = 3, attached_node = 1, not_in_creative_inventory = 1},
-	sounds = default.node_sound_glass_defaults(),
-	mesecons = {receptor = {
-		state = mesecon.state.on,
-		rules = mesecon.rules.wallmounted_get,
-	}},
-	on_blast = mesecon.on_blastnode
-})
-
--- Solar Panel
-minetest.register_node("mesecons_solarpanel:solar_panel_off", {
+mesecon.register_node("mesecons_solarpanel:solar_panel", {
 	description = "Solar Panel",
 	drawtype = "nodebox",
-	tiles = { "jeija_solar_panel.png" },
-	inventory_image = "jeija_solar_panel.png",
-	wield_image = "jeija_solar_panel.png",
+	tiles = {"mesecons_solarpanel.png"},
+	inventory_image = "mesecons_solarpanel.png",
+	wield_image = "mesecons_solarpanel.png",
 	paramtype = "light",
 	paramtype2 = "wallmounted",
 	is_ground_content = false,
 	node_box = {
 		type = "wallmounted",
-		wall_bottom	= { -8/16, -8/16, -8/16,  8/16, -2/16, 8/16 },
-		wall_top	= { -8/16,  2/16, -8/16,  8/16,  8/16, 8/16 },
-		wall_side	= { -2/16, -8/16, -8/16, -8/16,  8/16, 8/16 }
+		wall_bottom	= {-0.5, -0.5, -0.5,  0.5, -1/7, 0.5},
+		wall_top	= {-0.5,  1/7, -0.5,  0.5,  0.5, 0.5},
+		wall_side	= {-1/7, -0.5, -0.5, -0.5,  0.5, 0.5}
 	},
-	groups = {dig_immediate = 3, attached_node = 1},
 	sounds = default.node_sound_glass_defaults(),
+	on_blast = mesecon.on_blastnode
+},{
+	groups = {dig_immediate = 3, attached_node = 1},
 	mesecons = {receptor = {
 		state = mesecon.state.off,
 		rules = mesecon.rules.wallmounted_get
-	}},
-	on_blast = mesecon.on_blastnode
+	}}
+},{
+	groups = {dig_immediate = 3, attached_node = 1, not_in_creative_inventory = 1},
+	mesecons = {receptor = {
+		state = mesecon.state.on,
+		rules = mesecon.rules.wallmounted_get
+	}}
 })
 
 minetest.register_craft({
@@ -58,30 +40,21 @@ minetest.register_craft({
 })
 
 minetest.register_abm({
-	nodenames = {"mesecons_solarpanel:solar_panel_off"},
+	label = "Solar Panel On/Off",
+	nodenames = {
+		"mesecons_solarpanel:solar_panel_off",
+		"mesecons_solarpanel:solar_panel_on"
+	},
 	interval = 3,
 	chance = 1,
 	catch_up = false,
 	action = function(pos, node)
 		local light = minetest.get_node_light(pos)
-
-		if light >= 10 then
+		if light >= 10 and node.name == "mesecons_solarpanel:solar_panel_off" then
 			node.name = "mesecons_solarpanel:solar_panel_on"
 			minetest.swap_node(pos, node)
 			mesecon.receptor_on(pos, mesecon.rules.wallmounted_get(node))
-		end
-	end
-})
-
-minetest.register_abm({
-	nodenames = {"mesecons_solarpanel:solar_panel_on"},
-	interval = 3,
-	chance = 1,
-	catch_up = false,
-	action = function(pos, node)
-		local light = minetest.get_node_light(pos)
-
-		if light < 10 then
+		elseif light < 10 and node.name == "mesecons_solarpanel:solar_panel_on" then
 			node.name = "mesecons_solarpanel:solar_panel_off"
 			minetest.swap_node(pos, node)
 			mesecon.receptor_off(pos, mesecon.rules.wallmounted_get(node))
@@ -89,6 +62,5 @@ minetest.register_abm({
 	end
 })
 
--- Solar panel
 minetest.register_alias("mesecons_solarpanel:solar_panel_inverted_on", "mesecons_solarpanel:solar_panel_on")
 minetest.register_alias("mesecons_solarpanel:solar_panel_inverted_off", "mesecons_solarpanel:solar_panel_off")
