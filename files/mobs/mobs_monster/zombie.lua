be02c70c002cb62be311f262a13c188d7bb4473d
MoNTE48 <MoNTE48@mail.ua>
2020-04-28 21:32:12 +0200

    mobs_monster: add Giant Zombie

--- files/mobs/mobs_monster/zombie.lua
+++ files/mobs/mobs_monster/zombie.lua
@@ -42,6 +42,57 @@ mobs:register_mob("mobs_monster:zombie", {
 	end
 })
 
+mobs:register_mob("mobs_monster:zombie_giant", {
+	type = "monster",
+	visual = "mesh",
+	mesh = "mobs_zombie.b3d",
+	textures = {"mobs_zombie.png"},
+	collisionbox = {-0.75, -2.5, -0.75, 0.75, 2, 0.75},
+	visual_size = {x = 2.5, y = 2.5},
+	animation = {
+		speed_normal = 10,	speed_run = 15,
+		stand_start = 0,	stand_end = 79,
+		walk_start = 168,	walk_end = 188,
+		run_start = 168,	run_end = 188
+	},
+	makes_footstep_sound = true,
+	sounds = {
+		random = "mobs_zombie.1",
+		war_cry = "mobs_zombie.3",
+		attack = "mobs_zombie.2",
+		damage = "mobs_zombie_hit",
+		death = "mobs_zombie_death"
+	},
+	hp_min = 30,
+	hp_max = 50,
+	water_damage = 2,
+	light_damage = 2,
+	damage = 5,
+	group_attack = false,
+	attack_npcs = true,
+	view_range = 25,
+	walk_chance = 50,
+	walk_velocity = 1.5,
+	run_velocity = 3,
+	jump = true,
+	drops = function(pos)
+		if rawget(_G, "experience") then
+			experience.add_orb(math.random(6, 10), pos)
+		end
+		return {
+			{name = "mobs_monster:rotten_flesh"},
+			{name = "mobs_monster:rotten_flesh"},
+			{name = "mobs_monster:rotten_flesh"},
+			{name = "mobs_monster:rotten_flesh", chance = 2},
+			{name = "mobs_monster:rotten_flesh", chance = 2},
+			{name = "mobs_monster:rotten_flesh", chance = 2},
+			{name = "mobs_monster:rotten_flesh", chance = 3},
+			{name = "mobs_monster:rotten_flesh", chance = 3},
+			{name = "mobs_monster:rotten_flesh", chance = 3}
+		}
+	end
+})
+
 mobs:spawn({
 	name = "mobs_monster:zombie",
 	nodes = mobs_monster.spawn_nodes,
@@ -49,4 +100,12 @@ mobs:spawn({
 	chance = 15000
 })
 
+mobs:spawn({
+	name = "mobs_monster:zombie_giant",
+	nodes = mobs_monster.spawn_nodes,
+	max_light = 4,
+	chance = 35000
+})
+
 mobs:register_egg("mobs_monster:zombie", "Zombie Head", "zombie_head.png")
+mobs:register_egg("mobs_monster:zombie_giant", "Giant Zombie Head", "zombie_head.png")
