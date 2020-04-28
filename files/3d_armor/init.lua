5876dbf275f236e633b83ddc346f9134408565fc
MultiCraft <support@multicraft.world>
2020-04-28 21:30:13 +0200

    3d_armor: fix armor inventory saving

--- files/3d_armor/init.lua
+++ files/3d_armor/init.lua
@@ -3,35 +3,25 @@ local modpath = minetest.get_modpath("3d_armor")
 dofile(modpath .. "/api.lua")
 dofile(modpath .. "/armor.lua")
 
--- Register Callbacks
-
-local function handle_inventory(player)
-	if player and player:is_player() then
-		armor:save_armor_inventory(player)
-		armor:set_player_armor(player)
-		sfinv.set_player_inventory_formspec(player)
-	end
-end
-
 minetest.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
 	local armor_inv = minetest.create_detached_inventory(name .. "_armor", {
 		allow_put = function(_, _, index, stack)
 			local item = minetest.registered_tools[stack:get_name()]
-			if not item or not item.groups then
-				return 0
-			end
-			if item.groups.armor_head  and index == 1 then
-				return 1
-			end
-			if item.groups.armor_torso and index == 2 then
-				return 1
-			end
-			if item.groups.armor_legs  and index == 3 then
-				return 1
-			end
-			if item.groups.armor_feet  and index == 4 then
-				return 1
+			local group = item and item.groups
+			if group then
+				if group.armor_head  and index == 1 then
+					return 1
+				end
+				if group.armor_torso and index == 2 then
+					return 1
+				end
+				if group.armor_legs  and index == 3 then
+					return 1
+				end
+				if group.armor_feet  and index == 4 then
+					return 1
+				end
 			end
 			return 0
 		end,
@@ -39,10 +29,10 @@ minetest.register_on_joinplayer(function(player)
 			return 0
 		end,
 		on_put = function(_, _, _, _, player)
-			handle_inventory(player)
+			armor:handle_inventory(player)
 		end,
 		on_take = function(_, _, _, _, player)
-			handle_inventory(player)
+			armor:handle_inventory(player)
 		end
 	}, name)
 
@@ -56,7 +46,7 @@ minetest.register_on_joinplayer(function(player)
 	}
 	armor.textures[name] = {armor = "blank.png"}
 	minetest.after(1, function()
-		handle_inventory(player)
+		armor:handle_inventory(player)
 	end)
 end)
 
@@ -82,7 +72,7 @@ minetest.register_on_dieplayer(function(player)
 			end
 			armor_inv:set_list("armor", {})
 		end
-		handle_inventory(player)
+		armor:handle_inventory(player)
 	end
 end)
 
