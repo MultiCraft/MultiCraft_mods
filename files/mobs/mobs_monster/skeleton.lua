fda57d8121bf83a2c1626c5bc1f632d9d22a2802
MoNTE48 <MoNTE48@mail.ua>
2020-04-28 21:23:41 +0200

    Remove compatibility replace older .x models

--- files/mobs/mobs_monster/skeleton.lua
+++ files/mobs/mobs_monster/skeleton.lua
@@ -39,17 +39,6 @@ mobs:register_mob("mobs_monster:skeleton", {
 			{name = "default:bone", chance = 2},
 			{name = "default:bone", chance = 2}
 		}
-	end,
-
-	after_activate = function(self)
-		-- replace skeleton using the old directx model
-		if self.mesh == "mobs_zombie.x" then
-		local pos = self.object:get_pos()
-		if pos then
-			minetest.add_entity(pos, self.name)
-			self.object:remove()
-		end
-		end
 	end
 })
 
