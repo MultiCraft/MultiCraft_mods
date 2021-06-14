c05307e12b157dc60ab738df81e78925ae52cdf1
MoNTE48 <MoNTE48@mail.ua>
2021-06-15 00:31:13 +0200

    Farming: update

--- files/farming/farming/README.txt
+++ files/farming/farming/README.txt
@@ -8,3 +8,16 @@ Originally by PilzAdam (MIT)
 webdesigner97 (MIT)
 Various Minetest developers and contributors (MIT)
 MultiCraft Development Team (MIT)
+
+Authors of media (textures and models)
+--------------------------------------
+Copyright (C) MultiCraft Development Team
+
+farming_bread.png
+farming_flour.png
+farming_string.png
+farming_wheat.png
+farming_wheat_seed.png
+
+Graphics in this mod is NOT free and can be used only as part of the official MultiCraft build.
+Allowed to be used in non-official builds ONLY for personal use.
--- files/farming/farming_addons/init.lua
+++ files/farming/farming_addons/init.lua
@@ -9,7 +9,6 @@ if translator and not minetest.is_singleplayer() then
 		farming_addons.S = intllib.make_gettext_pair()
 	end
 end
-local S = farming_addons.S
 
 local path = minetest.get_modpath("farming_addons")
 local farming = {
@@ -22,42 +21,6 @@ for _, name in pairs(farming) do
 	dofile(path .. "/" .. name .. ".lua")
 end
 
---
--- Crafting recipes & items
---
-
--- Soup Bowl
-minetest.register_craftitem("farming_addons:bowl", {
-	description = S"Empty Bowl",
-	inventory_image = "farming_addons_bowl.png",
-	groups = {food = 1}
-})
-
-minetest.register_craft({
-	output = "farming_addons:bowl 3",
-	recipe = {
-		{"group:wood", "", "group:wood"},
-		{"", "group:wood", ""}
-	}
-})
-
--- Hog Stew
-minetest.register_craftitem("farming_addons:bowl_hog_stew", {
-	description = S"Stewed Pork Bowl",
-	inventory_image = "farming_addons_hog_stew.png",
-	on_use = minetest.item_eat(8, "farming_addons:bowl"),
-	groups = {food = 1}
-})
-
-minetest.register_craft({
-	output = "farming_addons:bowl_hog_stew",
-	recipe = {
-		{"", "mobs:pork_raw", ""},
-		{"farming_addons:carrot", "farming_addons:potato_baked", "flowers:mushroom_brown"},
-		{"", "farming_addons:bowl", ""}
-	}
-})
-
 minetest.after(1, function()
 	-- Add bonemeal support
 	if minetest.global_exists("bonemeal") then
@@ -68,14 +31,14 @@ minetest.after(1, function()
 			{"farming_addons:pumpkin_", 8, "farming_addons:seed_pumpkin"}
 		})
 	end
-
-	-- Register dungeon loot
-	if minetest.global_exists("dungeon_loot") then
-		dungeon_loot.register({
-			{name = "farming_addons:seed_carrot", chance = 0.5, count = {2, 4}},
-			{name = "farming_addons:cocoa_3", chance = 0.5, count = {2, 4}},
-			{name = "farming_addons:seed_melon", chance = 0.5, count = {1, 2}},
-			{name = "farming_addons:seed_pumpkin", chance = 0.5, count = {1, 2}}
-		})
-	end
 end)
+
+-- Register dungeon loot
+if minetest.global_exists("dungeon_loot") then
+	dungeon_loot.register({
+		{name = "farming_addons:seed_carrot", chance = 0.5, count = {2, 4}},
+		{name = "farming_addons:cocoa_3", chance = 0.5, count = {2, 4}},
+		{name = "farming_addons:seed_melon", chance = 0.5, count = {1, 2}},
+		{name = "farming_addons:seed_pumpkin", chance = 0.5, count = {1, 2}}
+	})
+end
