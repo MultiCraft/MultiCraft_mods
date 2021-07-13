48d9a72eafee089e4d6c5aa9b3c0ef40b9c1812a
MoNTE48 <MoNTE48@mail.ua>
2021-07-13 19:20:40 +0300

    Player_api: add model preview
    Co-Authored-By: Jean-Patrick Guerrero <kilbith@users.noreply.github.com>
    Co-Authored-By: luk3yx <luk3yx@users.noreply.github.com>

--- files/player/player_api/sscsm.lua
+++ files/player/player_api/sscsm.lua
@@ -2,11 +2,13 @@
 -- the inventory.
 
 player_api = {}
+local fmt = string.format
 
+-- This doesn't have nearly as many features as the server-side preview_model
+-- function.
 local preview
-function player_api.preview()
-	assert(preview)
-	return preview
+function player_api.preview_model(_, x, y, w, h)
+	return fmt("model[%f,%f;%f,%f;%s]", x, y, w, h, assert(preview))
 end
 
 sscsm.register_on_com_receive("player_api:preview", function(msg)
