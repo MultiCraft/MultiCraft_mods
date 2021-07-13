5c8dd756af349e87101f82a9f324747b706ef729
MultiCraft <support@multicraft.world>
2021-07-13 19:21:41 +0300

    Player_api: add compat_mode API

--- files/player/player_api/sscsm.lua
+++ files/player/player_api/sscsm.lua
@@ -11,6 +11,11 @@ function player_api.preview_model(_, x, y, w, h)
 	return fmt("model[%f,%f;%f,%f;%s]", x, y, w, h, assert(preview))
 end
 
+-- Assume clients with SSCSM are either MC2 or MT 5.4+
+function player_api.compat_mode()
+	return false
+end
+
 sscsm.register_on_com_receive("player_api:preview", function(msg)
 	preview = msg
 end)
