d2d8a94cd12b286bb1d5e9ef95268c5a80b0719d
MultiCraft <support@multicraft.world>
2021-09-17 10:57:11 +0200

    Player_api: update wieldview for some nodes

--- files/player/player_api/wieldview.lua
+++ files/player/player_api/wieldview.lua
@@ -11,33 +11,39 @@ local function prepare()
 		local inv_img = def.inventory_image
 		local tiles = def.tiles
 		local groups = def.groups
-		if inv_img and inv_img ~= "" then
-			if (minetest.registered_tools[name] or (groups.wieldview == 2)) and
-					not (groups.wieldview == 3) then
-				wield_tiles[name] = inv_img
-			else
-				wield_tiles[name] = inv_img .. "^[transformR270"
-			end
-		elseif tiles and type(tiles[1]) == "string" and tiles[1] ~= "" then
-			local drawtype = def.drawtype
-			if drawtype and
-					(drawtype == "normal" or
-					 drawtype == "liquid" or
-					 drawtype:sub(1, 8) == "allfaces" or
-					 drawtype:sub(1, 5) == "glass") or
-					(groups.wieldview == 4) then
-				if tiles[3] ~= "" and type(tiles[3]) == "string" then
-					wield_cubes[name] = tiles[3]
+		local not_in_inv = groups.not_in_creative_inventory
+		if not not_in_inv or not_in_inv < 1 then
+			if inv_img and inv_img ~= "" then
+				if (minetest.registered_tools[name] or (groups.wieldview == 2)) and
+						not (groups.wieldview == 3) then
+					wield_tiles[name] = inv_img
 				else
-					wield_cubes[name] = tiles[1]
+					wield_tiles[name] = inv_img .. "^[transformR270"
 				end
-			else
-				if (tiles[6] ~= "" and type(tiles[6]) == "string") then
-					wield_tiles[name] = tiles[6]
-				elseif (tiles[3] ~= "" and type(tiles[3]) == "string") then
-					wield_tiles[name] = tiles[3]
+			elseif tiles and type(tiles[1]) == "string" and tiles[1] ~= "" then
+				local wield_cube = def.wield_cube
+				local drawtype = def.drawtype
+				if wield_cube and wield_cube ~= "" then
+					wield_cubes[name] = wield_cube
+				elseif drawtype and
+						(drawtype == "normal" or
+						 drawtype == "liquid" or
+						 drawtype:sub(1, 8) == "allfaces" or
+						 drawtype:sub(1, 5) == "glass") or
+						(groups.wieldview == 4) then
+					if tiles[3] ~= "" and type(tiles[3]) == "string" then
+						wield_cubes[name] = tiles[3]
+					else
+						wield_cubes[name] = tiles[1]
+					end
 				else
-					wield_tiles[name] = tiles[1]
+					if (tiles[6] ~= "" and type(tiles[6]) == "string") then
+						wield_tiles[name] = tiles[6]
+					elseif (tiles[3] ~= "" and type(tiles[3]) == "string") then
+						wield_tiles[name] = tiles[3]
+					else
+						wield_tiles[name] = tiles[1]
+					end
 				end
 			end
 		end
