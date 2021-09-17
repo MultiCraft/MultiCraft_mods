8007b301609ae34f15bd2e02345ee6c438554dea
MultiCraft <support@multicraft.world>
2021-09-17 10:36:10 +0200

    Farming: update and add MVPS support

--- files/farming/farming_addons/api.lua
+++ files/farming/farming_addons/api.lua
@@ -1,6 +1,12 @@
+local random = math.random
+
+-- how often node timers for plants will tick, +/- some random value
+function farming_addons.tick(pos)
+	minetest.get_node_timer(pos):start(random(180, 256))
+end
 -- how often a growth failure tick is retried (e.g. too dark)
-function tick(pos)
-	minetest.get_node_timer(pos):start(math.random(256, 768))
+function farming_addons.tick_again(pos)
+	minetest.get_node_timer(pos):start(math.random(96, 256))
 end
 
 local function check_free(pos)
@@ -78,11 +84,11 @@ function farming_addons.grow_block(pos, connect, rotate)
 
 	-- plant is closed from all sides
 	if #spawn_pos < 1 then
-		tick(pos)
+		farming_addons.tick_again(pos)
 		return
 	else
 		-- pick random from the open sides
-		local pick_random = math.random(#spawn_pos)
+		local pick_random = random(#spawn_pos)
 
 		for k, v in pairs(spawn_pos) do
 			if k == pick_random then
@@ -94,7 +100,7 @@ function farming_addons.grow_block(pos, connect, rotate)
 	-- check light
 	local light = minetest.get_node_light(pos)
 	if not light or light < 12 then
-		tick(pos)
+		farming_addons.tick_again(pos)
 		return
 	end
 
@@ -117,4 +123,21 @@ function farming_addons.grow_block(pos, connect, rotate)
 		minetest.get_meta(block_pos):set_string("parent", spos)
 	end
 	return
+end
+
+function farming_addons.dig_node(meta, node, swap)
+	local parent = meta.fields.parent
+	local parent_pos_from_child = minetest.string_to_pos(parent)
+	local parent_node
+
+	-- make sure we have position
+	if parent_pos_from_child then
+		parent_node = minetest.get_node(parent_pos_from_child)
+	end
+
+	-- tick parent if parent stem still exists
+	if parent_node and parent_node.name == node then
+		minetest.swap_node(parent_pos_from_child, {name = swap})
+		farming_addons.tick(parent_pos_from_child)
+	end
 end
