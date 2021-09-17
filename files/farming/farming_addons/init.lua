7d8f1c16376532c9a5a2ebe55b1a8f625e871855
MultiCraft <support@multicraft.world>
2021-09-17 10:29:26 +0200

    Farming_addons: cleanup

--- files/farming/farming_addons/init.lua
+++ files/farming/farming_addons/init.lua
@@ -1,7 +1,7 @@
 farming_addons = {}
 
 local translator = minetest.get_translator
-farming_addons.S = translator and translator("farming") or intllib.make_gettext_pair()
+farming_addons.S = translator and translator("farming_addons") or intllib.make_gettext_pair()
 
 if translator and not minetest.is_singleplayer() then
 	local lang = minetest.settings:get("language")
@@ -13,11 +13,10 @@ end
 local path = minetest.get_modpath("farming_addons")
 local farming = {
 	"api", "seeds",
-	"carrot", "cocoa",
-	"watermelon", "pumpkin"
+	"cocoa", "watermelon", "pumpkin"
 }
 
-for _, name in pairs(farming) do
+for _, name in ipairs(farming) do
 	dofile(path .. "/" .. name .. ".lua")
 end
 
@@ -25,7 +24,6 @@ minetest.after(1, function()
 	-- Add bonemeal support
 	if minetest.global_exists("bonemeal") then
 		bonemeal.add_crop({
-			{"farming_addons:carrot_", 4, "farming_addons:seed_carrot"},
 			{"farming_addons:cocoa_", 3, "farming_addons:seed_cocoa"},
 			{"farming_addons:melon_", 8, "farming_addons:seed_melon"},
 			{"farming_addons:pumpkin_", 8, "farming_addons:seed_pumpkin"}
@@ -36,7 +34,6 @@ end)
 -- Register dungeon loot
 if minetest.global_exists("dungeon_loot") then
 	dungeon_loot.register({
-		{name = "farming_addons:seed_carrot", chance = 0.5, count = {2, 4}},
 		{name = "farming_addons:cocoa_3", chance = 0.5, count = {2, 4}},
 		{name = "farming_addons:seed_melon", chance = 0.5, count = {1, 2}},
 		{name = "farming_addons:seed_pumpkin", chance = 0.5, count = {1, 2}}
