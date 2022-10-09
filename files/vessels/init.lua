7e85e53ec48bf0a5ac57b1b9e8cf94190a20bdae
MultiCraft <support@multicraft.world>
2022-10-09 15:58:18 +0300

    Vessels: update

--- files/vessels/init.lua
+++ files/vessels/init.lua
@@ -1,11 +1,14 @@
-local S = minetest.get_translator_auto(true)
+local S = minetest.get_translator("vessels")
+
+local VER = "3"
 
 local vessels_shelf_formspec =
 	default.gui ..
 	"item_image[0,-0.1;1,1;vessels:shelf]" ..
 	"label[0.9,0.1;" .. S("Potion Shelf") .. "]" ..
 	"list[context;vessels;0,1;9,2;]" ..
-	"list[context;split;8,3.14;1,1;]" ..
+	"item_image[8,3.2;1,1;default:split_cell]" ..
+	"list[context;split;8,3.2;1,1;]" ..
 	"listring[context;vessels]" ..
 	"listring[current_player;main]"
 
@@ -13,6 +16,11 @@ local function update_vessels_shelf(pos)
 	local meta = minetest.get_meta(pos)
 	local inv = meta:get_inventory()
 	local invlist = inv:get_list("vessels")
+	if not invlist then
+		inv:set_size("vessels", 9 * 2)
+		inv:set_size("split", 1)
+		invlist = inv:get_list("vessels")
+	end
 
 	local formspec = vessels_shelf_formspec
 	-- Inventory slots overlay
@@ -43,7 +51,7 @@ local function update_vessels_shelf(pos)
 		meta:set_string("infotext", S"Empty Potion Shelf")
 	else
 		meta:set_string("infotext", S"Potion Shelf" .. "\n(" ..
-			S("Potions: @1", n_potions) .. ", " .. S("Bottles @1:", n_empty) .. ")")
+			S("Potions: @1", n_potions) .. ", " .. S("Bottles: @1", n_empty) .. ")")
 	end
 end
 
@@ -65,7 +73,7 @@ minetest.register_node("vessels:shelf", {
 		inv:set_size("vessels", 9 * 2)
 		inv:set_size("split", 1)
 		update_vessels_shelf(pos)
-		meta:set_string("version", "2")
+		meta:set_string("version", VER)
 	end,
 	can_dig = function(pos)
 		local inv = minetest.get_meta(pos):get_inventory()
@@ -152,13 +160,13 @@ minetest.register_craft({
 -- LBM for updating Potion Shelf
 minetest.register_lbm({
 	label = "Potion Shelf updater",
-	name = "vessels:shelf_updater",
+	name = "vessels:shelf_updater_v" .. VER,
 	nodenames = "vessels:shelf",
 	action = function(pos)
 		local meta = minetest.get_meta(pos)
-		if meta:get_string("version") ~= "2" then
+		if meta:get_string("version") ~= VER then
 			update_vessels_shelf(pos)
-			meta:set_string("version", "2")
+			meta:set_string("version", VER)
 		end
 	end
 })
